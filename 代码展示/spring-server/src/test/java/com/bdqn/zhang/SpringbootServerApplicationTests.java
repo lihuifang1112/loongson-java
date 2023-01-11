6ef323cac71c457fabe5f417671b1ba85ad2d3f0
lihuifang1112 <122155172+lihuifang1112@users.noreply.github.com>
2023-01-11 16:42:51 +0800

    Add files via upload

---
+++ 代码展示/spring-server/src/test/java/com/bdqn/zhang/SpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.zhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
